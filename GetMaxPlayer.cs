d93b2ba1141e8950d8ae06f0de2d807069e4967a
CEZA - exelent#3614 <93098721+cezacs1@users.noreply.github.com>
2022-12-31 00:25:21 +0300

    Create GetMaxPlayer.cs

---
+++ GetMaxPlayer.cs
@@ -0,0 +1,18 @@
+        private static void automax()
+        {
+            new Thread
+                while (*cheat run*)
+                {
+                    if (!connected) contiune;
+
+                    for (int i = 0; i < 64; i++)
+                    {
+                       if (entity health == 0) continue;
+                       last++;
+                    }
+                    maxplayer = last + 1; // +1 = tolerans
+                    
+                    Thread.Sleep(5000);
+                }
+            Start;
+        }
